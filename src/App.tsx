d9b2176af258e3170946ff06260de1bcf0d7bd36
Divanio Silva <dcsilva@mail.ru>
2024-08-27 11:47:24 +0100

    Button: set the default and permitted values

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
     return (
         <div>
-            <Button color={"primary"} onClick={() => console.log("clicked")}>My button</Button>
+            <Button onClick={() => console.log("clicked")}>My button</Button>
         </div>
     );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,13 @@
 
 interface Props {
     children: string;
-    color: string;
+    color?: 'primary' | 'secondary' | 'danger';
+    //making this property optional with question mark
+    //'primary' | 'secondary' | 'danger' -> these approach will force for this variable to accept only these values that are already set
     onClick: () => void;
 }
 
-const Button = ({children, onClick, color} : Props) => {
+const Button = ({children, onClick, color = 'primary'} : Props) => {
     return (
         <button type="button" className={"btn btn-"+color} onClick={onClick}>{children}</button>
     )
